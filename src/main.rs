73ae99694c91c48b8c30d2df95ccb91de37ad79b
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2026-04-01 19:43:35 +0530

    feat: change to google's dns, use proberesult enum for results

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,13 @@ use std::mem::MaybeUninit;
 use std::net::{Ipv4Addr, SocketAddrV4};
 use std::time::Duration;
 
-fn probe(target: Ipv4Addr, ttl: u32) -> std::io::Result<Option<Ipv4Addr>> {
+enum ProbeResult {
+    Hop(Ipv4Addr),     // Type 11 - Time Exceeded
+    Reached(Ipv4Addr), // Type 3 - Destination Unreachable
+    Timeout,           // No reply
+}
+
+fn probe(target: Ipv4Addr, ttl: u32) -> std::io::Result<ProbeResult> {
     // UDP socket to send the probe
     let send_sock = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
     send_sock.set_ttl_v4(ttl)?;
@@ -16,7 +22,7 @@ fn probe(target: Ipv4Addr, ttl: u32) -> std::io::Result<Option<Ipv4Addr>> {
     )?;
     recv_sock.set_read_timeout(Some(Duration::from_secs(2)))?;
 
-    // Sned UDP packet to high port (33434)
+    // Send UDP packet to high port (33434)
     let dest = SockAddr::from(SocketAddrV4::new(target, 33434));
     send_sock.send_to(&[0u8; 32], &dest)?;
 
@@ -27,28 +33,34 @@ fn probe(target: Ipv4Addr, ttl: u32) -> std::io::Result<Option<Ipv4Addr>> {
             // Safety:: recv wrote n bytes into buf
             let buf: &[u8] = unsafe { std::slice::from_raw_parts(buf.as_ptr() as *const u8, n) };
             // IP Header is first 20 bytes, source IP is at bytes 12-16
-            if buf.len() >= 20 {
+            if buf.len() >= 21 {
                 let ip = Ipv4Addr::new(buf[12], buf[13], buf[14], buf[15]);
-                Ok(Some(ip))
+                match buf[20] {
+                    11 => Ok(ProbeResult::Hop(ip)),
+                    3 if ip == target => Ok(ProbeResult::Reached(ip)),
+                    3 => Ok(ProbeResult::Hop(ip)),
+                    _ => Ok(ProbeResult::Timeout),
+                }
             } else {
-                Ok(None)
+                Ok(ProbeResult::Timeout)
             }
         }
-        Err(_) => Ok(None),
+        Err(_) => Ok(ProbeResult::Timeout),
     }
 }
 
 fn main() -> std::io::Result<()> {
-    // Manually set `github.com`'s IP for now.
-    let target: Ipv4Addr = Ipv4Addr::new(20, 207, 73, 82);
-    for ttl in 1..=30 {
+    // Manually set `8.8.8.8` (Google DNS) for now.
+    let target: Ipv4Addr = Ipv4Addr::new(8, 8, 8, 8);
+    for ttl in 1..=60 {
         let hop = probe(target, ttl)?;
         match hop {
-            Some(ip) => println!("{:>2} {}", ttl, ip),
-            None => println!("{:>2} *", ttl),
-        }
-        if hop == Some(target) {
-            break;
+            ProbeResult::Hop(ip) => println!("{:>2} {}", ttl, ip),
+            ProbeResult::Reached(ip) => {
+                println!("{:>2} {}", ttl, ip);
+                break;
+            }
+            ProbeResult::Timeout => println!("{:>2} *", ttl),
         }
     }
     Ok(())
